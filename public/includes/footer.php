d216673e5f645a3a53bb7a5562c958eb7871fcd6
Ruan bertan <ruan.bertan@gmail.com>
2026-03-02 22:37:49 -0300

    revisao de data inicial do projeto

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,11 +1,11 @@
 <footer class="footer">
     <div class="footer-container">
         <div class="footer-left">
-            Sistema de Mínimas © <?php echo date('Y'); ?>
+            Sistema de Mínimas © 2026
         </div>
 
         <div class="footer-right">
-            Versão 1.0 | Desenvolvido por TI Interna
+            Versão 2.0 | Desenvolvido por TI Interna
         </div>
     </div>
 </footer>
