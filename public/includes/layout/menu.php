f1042fcaf1464d90bf08c660d3ad63af10e48c9a
Ruan Bertan da Rocha <e145905@mohawkbr.com>
2026-05-12 10:39:12 -0300

    fix: revisando caminho da tela no menu

--- public/includes/layout/menu.php
+++ public/includes/layout/menu.php
@@ -3,7 +3,7 @@
         <li><a href="/index.php">Dashboard</a></li>
         <li><a href="/includes/cadastro/cadastro.php">Cadastro</a></li>
         <li><a href="/includes/logs/fases.php">Fases</a></li>
-        <li><a href="/includes/relatorios/fotografo.php">Fotografo</a></li>
+        <li><a href="/includes/relatorios/fotografia.php">Fotografo</a></li>
         <li><a href="/includes/relatorios/manipulacao.php">Manipulação</a></li>
     </ul>
 </nav>
