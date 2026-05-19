0de1a240a998664fc28180e391c2e8c72e0d1d0b
Ruan Bertan da Rocha <e145905@mohawkbr.com>
2026-05-19 11:28:29 -0300

    fix: revisando tela de envio ao fotografo para um macro que vai centralizar tudo como pacote

--- public/includes/layout/menu.php
+++ public/includes/layout/menu.php
@@ -3,7 +3,6 @@
         <li><a href="/index.php">Dashboard</a></li>
         <li><a href="/includes/cadastro/cadastro.php">Cadastro</a></li>
         <li><a href="/includes/logs/fases.php">Fases</a></li>
-        <li><a href="/includes/relatorios/fotografo.php">Fotografo</a></li>
-        <li><a href="/includes/relatorios/manipulacao.php">Manipulação</a></li>
+        <li><a href="/includes/relatorios/pacote.php">Pacote</a></li>
     </ul>
 </nav>
