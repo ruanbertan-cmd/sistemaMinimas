66c537085f22e6ee34542b2f20945a306e0f6856
Ruan Bertan da Rocha <e145905@mohawkbr.com>
2026-06-22 13:18:53 -0300

    feat: quando a imagem for aprovada sera jogando informacoes do item em questao para alimentar outro site

--- public/includes/produto/aprovar_imagem.php
+++ public/includes/produto/aprovar_imagem.php
@@ -122,6 +122,110 @@ try {
         ");
         $stmtupdate->execute([$processoId]);
 
+        // Registrando imagem aprovada para ser processada e vinculada ao item no sistema de imagem
+        $stmtImagem = $pdo->prepare("
+            INSERT INTO SM_item_imagens (
+                item_id,
+                processo_id,
+                marca,
+                categoria_pt_br,
+                categoria_us,
+                categoria_es,
+                serie,
+                tamanho_nominal,
+                descricao,
+                codigo_barras_cx,
+                superficie_pt_br,
+                superficie_us,
+                superficie_es,
+                junta_assentamento,
+                abrasao_superficial,
+                variacao_tonalidade,
+                unidade_medida,
+                pecas_caixa,
+                m2_caixa,
+                peso_caixa,
+                coeficiente_atrito,
+                espessura,
+                grupo_absorcao,
+                item_ativo,
+                cor_pt_br,
+                cor_us,
+                cor_es,
+                recomendado_uso,
+                local_uso,
+                retificado,
+                polo,
+                lancamento,
+                estilo,
+                estilo_us,
+                estilo_es,
+                relevo,
+                rodape,
+                numeros_face,
+                tamanho_fabricacao,
+                codigo_item,
+                unidade_fabricacao,
+                monocalibre,
+                CicloLancamento,
+                tem_minimas,
+                status
+            )
+            SELECT 
+                SM_item_imagens.item_id,
+                SM_itens_processos.id AS processo_id,
+                SM_cadastros_itens_minimas.marca,
+                SM_cadastros_itens_minimas.categoria_pt_br,
+                SM_cadastros_itens_minimas.categoria_us,
+                SM_cadastros_itens_minimas.categoria_es,
+                SM_cadastros_itens_minimas.serie,
+                SM_cadastros_itens_minimas.tamanho_nominal,
+                SM_cadastros_itens_minimas.descricao,
+                SM_cadastros_itens_minimas.codigo_barras_cx,
+                SM_cadastros_itens_minimas.superficie_pt_br,
+                SM_cadastros_itens_minimas.superficie_us,
+                SM_cadastros_itens_minimas.superficie_es,
+                SM_cadastros_itens_minimas.junta_assentamento,
+                SM_cadastros_itens_minimas.abrasao_superficial,
+                SM_cadastros_itens_minimas.variacao_tonalidade,
+                SM_cadastros_itens_minimas.unidade_medida,
+                SM_cadastros_itens_minimas.pecas_caixa,
+                SM_cadastros_itens_minimas.m2_caixa,
+                SM_cadastros_itens_minimas.peso_caixa,
+                SM_cadastros_itens_minimas.coeficiente_atrito,
+                SM_cadastros_itens_minimas.espessura,
+                SM_cadastros_itens_minimas.grupo_absorcao,
+                SM_cadastros_itens_minimas.item_ativo,
+                SM_cadastros_itens_minimas.cor_pt_br,
+                SM_cadastros_itens_minimas.cor_us,
+                SM_cadastros_itens_minimas.cor_es,
+                SM_cadastros_itens_minimas.recomendado_uso,
+                SM_cadastros_itens_minimas.local_uso,
+                SM_cadastros_itens_minimas.retificado,
+                SM_cadastros_itens_minimas.polo,
+                SM_cadastros_itens_minimas.lancamento,
+                SM_cadastros_itens_minimas.estilo,
+                SM_cadastros_itensMinima.estilo_us,
+                SM_cadastros_itensMinima.estilo_es,
+                SM_cadastros_itensMinima.relevo,
+                SM_cadastros_itensMinima.rodape,
+                SM_cadastros_itensMinima.numeros_face,
+                SM_cadastros_itensMinima.tamanho_fabricacao,
+                SM_cadastros_itensMinima.codigo_item,
+                SM_cadastros_itensMinima.unidade_fabricacao,
+                SM_cadastros_itensMinima.monocalibre,
+                SM_cadastros_itensMinima.CicloLancamento,
+                1 AS tem_minimas,
+                1 AS status
+            FROM_SM_item_imagens
+            INNER JOIN SM_itens_processos 
+                ON SM_item_imagens.item_id = SM_itens_processos.item_id
+            INNER JOIN SM_cadastros_itens_minimas 
+                ON SM_item_imagens.item_id = SM_cadastros_itens_minimas.item_id
+            WHERE SM_item_imagens.id = ?
+        ");
+        $stmtImagem->execute([$imagemId]);
+
 
     } elseif ($acao === 'rejeitado') {
         $stmtupdate = $pdo->prepare("
